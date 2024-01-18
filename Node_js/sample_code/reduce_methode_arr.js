cb69df6840bac17599644f63e6a250c41ca86303
Bhupendra Mestry (Consultant) <bhupendram4u@gmail.com>
2024-01-18 15:31:09 +0530

    reduce menthode

---
+++ Node_js/sample_code/reduce_methode_arr.js
@@ -0,0 +1,17 @@
+const arr = [9, 3, 7, 11, 5, 13, 19, 47, 35];
+let op = {
+    "min": 0, "max": 0, "avg": 0, "sum": 0
+}
+function inner_reduce(acc, cur, indx) {
+    acc.min = (cur < acc.min) ? cur : (acc.min == 0) ? cur : acc.min
+    acc.max = (cur > acc.max) ? cur : acc.max;
+    acc.sum = acc.sum + cur;
+    acc.avg = acc.sum / indx + 1;
+    return acc;
+}
+
+let everying = arr.reduce(inner_reduce, op)
+
+
+console.log("##Use of Reduce Methode ##: \n")
+console.log(everying)
